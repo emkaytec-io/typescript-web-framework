b61d6015e70277a9d1d9cab3f07dff327ba01b8e
Josh Butler <josh@emkaytec.io>
2023-11-25 09:15:27 -0600

    Pre-refactor
    Mega User class before composition refactor

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import { User } from './models/User';
 
-const user = new User({
-  name: 'myname',
-  age: 20,
-});
+const user = new User({ id: 3 });
+user.fetch();
 
-user.on('change', () => console.log('Change #1'));
-user.on('change', () => console.log('Change #2'));
-user.on('save', () => console.log('Save was triggered'));
-
-user.trigger('hover');
+setTimeout(() => {
+  console.log(user);
+}, 4000);
